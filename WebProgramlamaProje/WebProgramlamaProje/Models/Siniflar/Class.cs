e6fbcd4ecd7182f2dae404ec38354f8cb06f396d
YaseminMZR <83133207+YaseminMZR@users.noreply.github.com>
2023-01-01 17:12:47 +0300

    Düzenleme

--- WebProgramlamaProje/WebProgramlamaProje/Models/Siniflar/Class.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebProgramlamaProje.Models.Siniflar
-{
-    public class Class
-    {
-        public IEnumerable<Urun> Deger1 { get; set; }
-        public IEnumerable<Detay> Deger2 { get; set; }
-    }
-}
